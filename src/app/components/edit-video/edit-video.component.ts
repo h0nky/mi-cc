70386a609859a10b02ea535bb7a6d40684eff344
Jenya Golovnov <93ave.blues@gmail.com>
2023-10-06 11:43:58 +0200

    styles adjustments

--- src/app/components/edit-video/edit-video.component.ts
+++ src/app/components/edit-video/edit-video.component.ts
@@ -73,7 +73,7 @@ export class EditVideoComponent implements OnInit {
       map(authors => combineUpdateFormPayload(authors, authorId, this.itemId, formData))
     )
     
-    const subscription = combinedPayload$.subscribe(payload => {
+    combinedPayload$.subscribe(payload => {
       this.videoService.setNewVideo(payload, authorId)
       .subscribe(response => this.handleResponse(response.ok));
     })
