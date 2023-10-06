8bdf333978001f4402efba0b04d79cf72b600611
Jenya Golovnov <93ave.blues@gmail.com>
2023-10-06 14:22:48 +0200

    new item id fix

--- src/app/utils/normalizeFormData.ts
+++ src/app/utils/normalizeFormData.ts
@@ -2,7 +2,7 @@ import { Video, VideoFormData } from "src/app/interfaces";
 
 export const normalizeFormData = (formData: VideoFormData, lastId: number): Video => {
     return {
-        id: lastId++ || 0,
+        id: lastId + 1,
         catIds: formData.selectedCategories,
         name: formData.videoTitle,
         formats: { 'one': { res: "1080p", size: 1000 }},
